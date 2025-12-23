dd734b7b25abce71c3e3f08cf637caefd2cd7be6
Thyagarajan K R <thyagarajan1234@gmail.com>
2025-12-23 13:00:38 +0530

    Added backend spring boot application files

---
+++ src/main/java/com/applications/virtual_assistant_backend/VirtualAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.applications.virtual_assistant_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applications/virtual_assistant_backend/VirtualAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applications.virtual_assistant_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
